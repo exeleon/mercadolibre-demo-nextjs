f796a3d7e814330dddcf51d396a68376ab9c071c
Manuel Lopera <mlopera@tomi.digital>
2021-06-18 21:59:43 -0500

    Added webmanifest and favicon

--- apps/site/pages/_app.tsx
+++ apps/site/pages/_app.tsx
@@ -11,8 +11,19 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Demo Mercadolibre</title>
         <meta name="description" content="Demo de un e-commerce utilizando el API de Mercadolibre" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta property="og:site_name" content="Demo Mercadolibre" />
+        <meta property="og:type" content="website" />
+
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png"/>
+
+        <link rel="manifest" href="site.webmanifest" />
+        <meta name="theme-color" content="#FFE600" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Demo Mercadolibre" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       </Head>
 
       <Toolbar />
